b12c4c1f9f8846116131004e29282eaef1302d9b
AllSafeCybercurity <skull32273227@gmail.com>
2021-07-21 16:47:17 -0700

    '02'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,307 @@
-<h1>API TEST</h1>
+<html>
+  <! -- layout design heavily borrowed from https://twitter.com/netspooky >
+
+  <head>
+    <title>O.MG</title>
+    <link rel="icon" type="" sizes="56x56" href="favicon.ico">
+    <meta charset="utf-8">
+    <meta name="viewport">    
+    <style>
+      body {
+        color: #FEFEFE;
+        background-color: #0c0d10;
+      }
+      
+      @font-face {
+        font-family: "gohu";
+        src: url("gohu.woff") format('woff');
+      }
+      
+      pre {
+        font-family: "gohu", "Lucida Console", monospace, Monaco;
+        font-size: 14px;
+        line-height: 1.0;
+		-webkit-text-size-adjust: 140%;
+      }
+      
+      a {
+        color: #d10000;
+        text-decoration: none;
+      }
+      
+      .container {
+        margin-right: 0px;
+        margin-left: 0px;
+      }
+      
+
+      @media (min-width: 992px) {
+        .container {
+          width: 970px;
+        }
+      }
+      
+      @media (min-width: 1200px) {
+        .container {
+          width: 1170px;
+        }
+      }
+    </style>
+  </head>
+
+   <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com" rel="noopener" target="_blank"><span class="blue-text"></span></a></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+  {
+  "symbols": [
+    {
+      "proName": "FOREXCOM:SPXUSD",
+      "title": "S&P 500"
+    },
+    {
+      "proName": "FOREXCOM:NSXUSD",
+      "title": "Nasdaq 100"
+    },
+    {
+      "proName": "FX_IDC:EURUSD",
+      "title": "EUR/USD"
+    },
+    {
+      "proName": "BITSTAMP:BTCUSD",
+      "title": "BTC/USD"
+    },
+    {
+      "proName": "BITSTAMP:ETHUSD",
+      "title": "ETH/USD"
+    }
+  ],
+  "showSymbolLogo": false,
+  "colorTheme": "dark",
+  "isTransparent": true,
+  "displayMode": "compact",
+  "locale": "en"
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+  
+  <body>
+    <center>
+	<div class='container'>
+      <div style="display: inline-block; text-align: left;"><pre><center> <img src="logo.jpg" width="100"></center>	
+┌------------------------------------------------------------------------------┐
+│<a href="main.html">       MAIN       </a>│<a href="product.html">   PRODUCT SETUP    </a>│<a href="#">      LOGIN       </a>│<a href="#">      REGISTER     </a>│
+└------------------------------------------------------------------------------┘
+
+┌------------------------------------------------------------------------------┐
+│<a href="main.html">       MAIN       </a>│<a href="product.html">   PRODUCT SETUP    </a>│<a href="#">      LOGIN       </a>│<a href="#">      REGISTER     </a>│
+└------------------------------------------------------------------------------┘
+
+┌──────────────────────────────────────────────────────────────────────────────┐ 
+│  <h3>Market Trend</h3><!-- Widget BEGIN -->                                  
+<div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <div class="tradingview-widget-copyright">
+    <a href="https://www.tradingview.com/markets/cryptocurrencies/prices-all/" rel="noopener" target="_blank"><span class="blue-text"></span></a></div>
+    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
+    {
+    "width": "600",
+    "height": "300",
+    "defaultColumn": "performance",
+    "screener_type": "crypto_mkt",
+    "displayCurrency": "USD",
+    "colorTheme": "dark",
+    "locale": "en",
+    "isTransparent": true
+  }
+    </script>
+  </div> <!-- TradingView Widget END -->                            
+
+│                                                                              │
+│                                                                              │
+│                                                                              │ 
+└──────────────────────────────────────────────────────────────────────────────┘ 
+
+┌──────────────────────────────────────────────────────────────────────────────┐
+│                                                                              │ 
+│                                   TRADE-EXCH                                 │ 
+│                                                                              │ 
+│                                                                              │ 
+│   Exch - ORDER TYPE ............................................... [<a href="dash.html"> BUY </a>]  │
+│                |                                                             │
+│                └--- ORDER QUANTITY USD .  ORDER PRICE                        │
+│                     ORDER TYPE                                               │ 
+│                                                                              │ 
+│   Exch - ORDER TYPE ............................................... [<a href="dash.html"> SELL </a>] │
+│                |                                                             │
+│                └--- TRADES QUANTITY USD .  ORDERBOOK                         │
+│                     SELL TYPE                                                │ 
+│                                                                              │
+│                                                                              │ 
+└──────────────────────────────────────────────────────────────────────────────┘ 
+
+┌──────────────────────────────────────────────────────────────────────────────┐
+│                                                                              │ 
+│                                   CONTENTS                                   │ 
+│                                                                              │ 
+│                                                                              │ 
+│  O.MG Cable - USB-C ............................................... [<a href="https://shop.hak5.org/products/o-mg-cable-usb-c"> BUY </a>]  │
+│                |                                                             │
+│                └--- Smaller. More powerful. Longer range.                    │
+│                     Standard or Keylogger Edition.                           │ 
+│                                                                              │ 
+│  O.MG Cable - C to C Directional .................................. [<a href="https://shop.hak5.org/products/o-mg-cable-c-to-c-directional"> BUY </a>]  │
+│                |                                                             │
+│                └--- Built for demos, training, & attacker controlled attacks.│
+│                     Explore new attacks on smartphones & tablets.            │
+│                     Standard or Keylogger Edition.                           │ 
+│                                                                              │ 
+│  O.MG Cable - USB-A ............................................... [<a href="https://shop.hak5.org/products/o-mg-cable-usb-a"> BUY </a>]  │
+│                |                                                             │
+│                └--- The original. Full wireless control in a USB cable.      │
+│                     Standard or Keylogger Edition.                           │ 
+│                                                                              │ 
+│  O.MG DemonSeedEDU ................................................ [<a href="https://shop.hak5.org/collections/mischief-gadgets/products/o-mg-demonseed-edu"> BUY </a>]  │
+│                |                                                             │
+│                └--- Educational kit for DIY implanting USB cables.           │
+│                                                                              │ 
+│  Malicious Cable Detector ......................................... [<a href="https://shop.hak5.org/collections/mischief-gadgets/products/malicious-cable-detector-by-o-mg"> BUY </a>]  │
+│                |                                                             │
+│                └--- We create the problem... and the solution.               │
+│                                                                              │ 
+└──────────────────────────────────────────────────────────────────────────────┘ 
+
+<script type="text/javascript" src="https://static.cryptowat.ch/assets/scripts/embed.bundle.js"></script>
+
+┌──────────────────────────────────────────────────────────────────────────────┐
+
+
+│                                                                              │ 
+└──────────────────────────────────────────────────────────────────────────────┘ 
+
+<div id='position'>
+  <p>Position</p>
+  <span id='position_size' class='big_text'>0</span><span class='small_text'>Contracts</span>
+  <span id='position_price' class='big_text'>0.00</span><span class='small_text'>Avg Price</span>
+  <span id='position_gain' class='big_text'>0.00</span><span class='small_text'>% Profit</span>
+  <span id='position_liquidation' class='big_text'>0.00</span><span class='small_text'>Liquidation Price</span>
+</div>
+<div id='order_ticket'>
+  <p>Order Ticket</p>
+  <ul>
+    <li id='selected_order'>market</li>
+    <li>limit</li>
+  </ul>
+  <div id='market_interface'>
+    <span class='left_side'>quantity</span><input id='market_quantity' placeholder='0'><span class='right_side'>USD</span>
+    <br>
+    <button id='market_buy'>BUY<br>100<br>MARKET</button>
+    <button id='market_sell'>SELL<br>100<br>MARKET</button>
+  </div>
+</div>
+</div>
+</div>
+  <div id='mainbar'>
+    <div id='chartdiv'></div>
+    <div id='order_interface'>
+      <table>
+        <tr>
+          <td>
+            <span class='small_text'>ORDER TYPE</span>
+            <select id='order_types'>
+              <option value='market'>Market</option>
+              <option value='limit'>Limit</option>
+            </select>
+          </td>
+          <td>
+            <span class='small_text'>ORDER QUANTITY USD</span>
+            <input type='text' value='10' id='order_qty'>
+          </td>
+          <td>
+            <span class='small_text'>ORDER PRICE</span>
+            <input type='text' value='Market' id='order_price' disabled>
+          </td>
+          <td>
+            <button id='order_buy'>
+              <span>
+                BUY<br>
+                <span id='buy_order_qty'>10</span><br>
+                <span id='buy_order_price'>@MARKET</span>
+              </span>
+            </button>
+            <button id='order_sell'>
+              <span>
+                SELL<br>
+                <span id='sell_order_qty'>10</span><br>
+                <span id='sell_order_price'>@MARKET</span>
+              </span>
+            </button>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div id='bottombar'>
+      <div id='tradelist'>
+        <p>
+          TRADES
+        </p>
+        <center>
+          <table id='trades'>
+            <tr>
+              <th>TYPE</th>
+              <th>AMOUNT</th>
+              <th>PRICE</th>
+              <th>TIME</th>
+            </tr>
+          </table>
+        </center>
+      </div>
+      <div id='orderbook'>
+        <p>
+          ORDERBOOK
+        </p>
+        <center>
+          <table>
+            <tr>
+              <th colspan=3>BIDS</th>
+              <th colspan=3>ASKS</th>
+            </tr>
+            <tr>
+              <th>DEPTH</th>
+              <th>AMOUNT</th>
+              <th>PRICE</th>
+              <th>PRICE</th>
+              <th>AMOUNT</th>
+              <th>DEPTH</th>
+            </tr>
+          </table>
+        </center>
+      </div>
+      <div id='orderlist'>
+        <p>
+          ORDERS
+        </p>
+        <center>
+          <table id='orders'>
+            <tr>
+              <th>SIDE</th>
+              <th>AMOUNT</th>
+              <th>PRICE</th>
+              <th>CANCEL</th>
+            </tr>
+          </table>
+        </center>
+      </div>
+    </div>
+  </div>
+
+ 
+</pre></div>
+
+</center>
+	</div>
+  </body>
+
+</html>
